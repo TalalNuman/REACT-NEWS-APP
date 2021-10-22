e418c13b0e80ba67f14d7ba72cd2ebd71ae7f4c8
TalalNuman <talalnuman1@gmail.com>
2021-10-23 02:19:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Newx from "./components/Newx";
+import Navbar from "./components/Navbar";
+import LoadingBar from "react-top-loading-bar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+export default class App extends Component {
+  state = {
+    progress: 0,
+  };
+  constructor(){
+    super();
+    this.setProgress =this.setProgress.bind(this)
+  }
+  setProgress
+  setProgress(progress) {
+    this.setState({
+      progress: progress
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <LoadingBar color="#f11946" progress={this.state.progress} />
+          <Switch>
+            <Route exact path="/business">
+              <Newx
+                setProgress={this.setProgress}
+                key="business"
+                category="business"
+              />
+            </Route>
+            <Route exact path="/entertainment">
+              <Newx
+                setProgress={this.setProgress}
+                key="entertainment"
+                category="entertainment"
+              />
+            </Route>
+            <Route exact path="/health">
+              <Newx
+                setProgress={this.setProgress}
+                key="health"
+                category="health"
+              />
+            </Route>
+            <Route exact path="/sports">
+              <Newx
+                setProgress={this.setProgress}
+                key="sports"
+                category="sports"
+              />
+            </Route>
+            <Route exact path="/science">
+              <Newx
+                setProgress={this.setProgress}
+                key="science"
+                category="science"
+              />
+            </Route>
+            <Route exact path="/technology">
+              <Newx
+                setProgress={this.setProgress}
+                key="technology"
+                category="technology"
+              />
+            </Route>
+            <Route exact path="/">
+              <Newx setProgress={this.setProgress} key="" category="general" />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
