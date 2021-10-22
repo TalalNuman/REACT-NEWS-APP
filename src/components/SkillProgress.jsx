6b143e961efdbe94882db64b483ff1583f134337
TalalNuman <talalnuman1@gmail.com>
2021-10-23 02:19:46 +0500

    Create SkillProgress.jsx

---
+++ src/components/SkillProgress.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SkillProgress() {
+    return (
+        <div>
+                {/* https://tailwindcomponents.com/component/circular-progress-bar-1  */}
+                 
+                    
+
+
+
+
+
+        </div>
+    )
+}
